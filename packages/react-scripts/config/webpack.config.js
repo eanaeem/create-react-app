0eae18a06ea62ad0dd2b0737f846486d306a9b39
EHTISHAM.ABBASI <Ehtisham.Abbasi@dentsuaegis.com>
2019-07-07 19:18:49 +0100

    added babel styled component plugin

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -383,8 +383,8 @@ module.exports = function(webpackEnv) {
                 ),
                 // @remove-on-eject-end
                 plugins: [
+                  require.resolve('babel-plugin-styled-components'),
                   [
-                    require.resolve('babel-plugin-styled-components'),
                     require.resolve('babel-plugin-named-asset-import'),
                     {
                       loaderMap: {
